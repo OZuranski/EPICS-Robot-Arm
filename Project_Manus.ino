ffb0186b1c6b77f6ebfe490301f12ec32266a250
OZuranski <owczski@gmail.com>
2025-04-09 12:54:40 -0400

    +servo claw, stepper5, comments
    added function for claw control with servo, added back stepper 5, and added more comments for both old and new code to try and make it more readable

--- Project_Manus.ino
+++ Project_Manus.ino
@@ -1,14 +1,17 @@
 #include <AccelStepper.h>
 #include <IBusBM.h>
+#include <Servo.h>
 
 // Define the stepper motors and their pins, -> stepper'n'(1, step pin, dir pin)
 AccelStepper stepper1(1, 2, 3);
 AccelStepper stepper2(1, 4, 5);
 AccelStepper stepper3(1, 6, 7);
 AccelStepper stepper4(1, 8, 9);
+AccelStepper stepper5(1, 10, 11);
 
+Servo myservo; // Servo for claw
 
-IBusBM ibus;
+IBusBM ibus;   // for controller comms
 
 void setup() {
   delay(500);
@@ -16,8 +19,9 @@ void setup() {
   stepper2.setMaxSpeed(650);  // Right Y
   stepper3.setMaxSpeed(650);  // Left Y
   stepper4.setMaxSpeed(900);  // Left X
-  
-  Serial.begin(115200);        // baud rate for serial comm with the controller
+  stepper5.setMaxSpeed(650);  // Left? dial
+  myservo.attach(12);         // Servo should be plugged into 12 pmw pin 
+  Serial.begin(115200);       // baud rate for serial comm with the controller
   ibus.begin(Serial1);
   delay(500);
 }
@@ -27,17 +31,20 @@ void loop() {
   controlStep(stepper2, 1); // Control stepper2 with channel 1 - Right Y
   controlStep(stepper3, 2); // Control stepper3 with channel 2 - Left Y
   controlStep(stepper4, 3); // Control stepper4 with channel 3 - Left X
-  // controlServo call would go here. I was going to write it but it wouldn't be worth it until I have an actual servo to test as I write  
+  controlStep(stepper5, 4); // Control stepper5 with channel 4 - Left dial
+  controlServo(5);          // Control myservo  with channel 5 - Right dial  
 }
 
 void controlStep(AccelStepper &stepper, int channel) {
-  int chVal = ibus.readChannel(channel) - 1502;
+
+  int chVal = ibus.readChannel(channel) - 1502; // centers value, so base position value = 0
   
   // Deadzone logic
-  if (chVal > -100 && chVal < 100) {
+  if (chVal > -50 && chVal < 50) {
     stepper.setSpeed(0); // Stop motor if in deadzone
-  } else {
-    int jVal = -(chVal * 1.5);
+  } 
+  else {
+    int jVal = -(chVal);
     
     stepper.setSpeed(jVal); // Set motor speed
     Serial.print("Channel "); Serial.print(channel); // print which channel with corresponding value being read, ONLY PRINTS IF THERE IS A READING
@@ -47,6 +54,24 @@ void controlStep(AccelStepper &stepper, int channel) {
   stepper.runSpeed(); // Move motor
 }
 
-/*void controlServo(int channel) {
+void controlServo(int channel) {
+  
+  static int prevAngle = -1; // used to store previous angle so that (on line 65) we don't print or try to move servo -
+  // - if values aren't actually being changed.
 
-}*/
+  int chVal = ibus.readChannel(channel) - 1502;
+  
+  int jVal = -(chVal * 1.5);
+  int angle = map(jVal, -755, 755, 0, 180); // Convert joystick val to angle from 0 to 180
+
+  //if (abs(prevAngle - angle) < 2) return;
+
+  if (angle != prevAngle) {   
+    Serial.print("Channel "); Serial.print(channel); // print which channel with corresponding value being read, ONLY PRINTS IF THERE IS A READING
+    // Prints joystick value and corresponding channel, but also the correlating angle for the servo to go to
+    Serial.print(": "); Serial.print(jVal); Serial.print(" Degrees: "); Serial.print(angle); Serial.println("°");  
+
+    myservo.write(angle); // Move servo to correct angle
+    prevAngle = angle;
+  }  
+}
